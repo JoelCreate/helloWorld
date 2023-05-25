4841cc1a0d5bc15d3a5638b55728abf2ffe27f4c
Joel Wiesner <j.wiesner88@gmail.com>
2023-05-24 18:01:53 -0700

    Created database using Firebase and connected it

---
+++ app.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings {
+    databaseURL = "https://helloworld-c09a1-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const messagesInDB = (databse, "messages")
