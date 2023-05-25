8eb59451db930498657909c02994e238796f6775
Joel Wiesner <j.wiesner88@gmail.com>
2023-05-24 18:13:37 -0700

    wired up input field and send button
    when you type in the input field and press send, your message is added to the Firebase DB.

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const appSettings {
-    databaseURL = "https://helloworld-c09a1-default-rtdb.firebaseio.com/"
+const appSettings = {
+    databaseURL: "https://helloworld-c09a1-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const messagesInDB = (databse, "messages")
+const messagesInDB = ref(database, "messages")
 
 const inputFieldEl = document.getElementById("input-field")
 const sendButtonEl = document.getElementById("send-btn")
 const messagesEl = document.getElementById("messages")
+
+function clearInputField(){
+    inputFieldEl.value = ""
+}
+
+sendButtonEl.addEventListener("click", function(){
+    let inputValue = inputFieldEl.value
+
+    push(messagesInDB, inputValue)
+
+    clearInputField()
+
+})
+
+onValue(messagesInDB)
